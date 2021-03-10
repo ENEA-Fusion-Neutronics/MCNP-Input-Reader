ab7fddabc2165e7bc3bdf156b0f3316af1f8168f
gmariano <giovanni.mariano@uniroma1.it>
2021-03-10 12:12:20 +0100

    Updated MCNPCell and lines - 10/03/2021

--- mcnp_input_reader/util/lines.py
+++ mcnp_input_reader/util/lines.py
@@ -27,7 +27,39 @@ def split_on_tag(string: str, tags = []):
     string_upper = string.upper()
     for tag in tags:
         mytag = tag.upper()
-        string_upper = string_upper.replace(mytag, '\n' + mytag).replace('*\n'+mytag, '\n*'+mytag)
+        string_upper = string_upper.replace(' ' + mytag, '\n' + mytag).replace('*' + mytag, '\n*' + mytag)
     return string_upper.splitlines()
 
+def get_comment_and_endline(input_string: str, start_line: int):
+    """
+    Remove the useless comment lines from the input description of the card 
+    
+    :param input_string: Must be a string, it's the card description
+    :param start_line: Must be an integer, it's the line number of the card in the mcnp input
+    :return: tuple (input_description, comment, end_line)
+    
+    """
 
+    input_string_splitted = input_string.splitlines()       
+    comment_lines = []
+    for l in reversed(input_string_splitted):
+        if l[0].lower() == 'c' or l.strip()[0] == '$':
+            comment_lines.append(l.strip())
+        else:
+            break
+    comment_list = [l[1:] for l in comment_lines if l[0] == '$']
+    if len(comment_list)==0:
+        for l in reversed(comment_lines):
+            comment_list.append(l[1:])
+            if len(l.split())>1:
+                break
+    comment=' '.join(comment_list).strip()
+    if comment.lower().replace('c', '').replace('-', '').strip():
+        len_comment_list = len(comment_list)
+    else:
+        comment = ''
+        len_comment_list = 0
+    len_description = len(input_string_splitted)-len(comment_lines)+len_comment_list
+    input_description = '\n'.join(input_string_splitted[:len_description])
+    end_line = start_line + len_description - 1
+    return input_description, comment, end_line
